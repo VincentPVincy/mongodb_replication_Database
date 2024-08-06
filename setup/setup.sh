ac8cb7c451869404d3182059adf5ebc2830c333e
vincentrider33 <vincentrider33@gmail.com>
2024-08-06 21:47:03 +0530

    Update .dockerignore, Dockerfile, and setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -13,15 +13,3 @@ echo "Running replicaSet.js"
 mongo --host mongo-rs0-1 --port 27017 ./replicaSet.js || { echo "Failed to run replicaSet.js"; exit 1; }
 
 echo "Replica set initiated"
-
-
-
-
-# #!/bin/bash
-# echo *************************************************************
-# echo Starting the replica set 
-# echo *************************************************************
-
-# sleep 20 | echo Sleeping 20 seconds
-
-# mongo mongodb://mongo-rs0-1:27017 replicaSet.js
